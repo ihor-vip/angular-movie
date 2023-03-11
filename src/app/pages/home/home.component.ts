982897df5c745d1e008449032cee33d245a12c28
ihor-vip <vip20140902@gmail.com>
2023-03-11 18:03:46 +0200

    create all services

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MovieApiServiceService} from "../../service/movie-api-service.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  constructor(private service: MovieApiServiceService) {
+  }
+  ngOnInit(): void {
+    this.bannerData();
+  }
 
+  bannerData() {
+    this.service.bannerApiData().subscribe((result) => {
+      console.log(result, 'bannerresult#')
+    })
+  }
 }
