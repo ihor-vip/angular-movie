f65b35ec592a2f8c29ebc0b665b5a93b35bef7b2
ihor-vip <vip20140902@gmail.com>
2023-03-13 11:19:49 +0200

    thriller movies home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,11 +6,11 @@ import {MovieApiServiceService} from "../../service/movie-api-service.service";
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
   constructor(private service: MovieApiServiceService) {
   }
 
-  bannerResult:any = [];
+  bannerResult: any = [];
   trendingMovieResult: any = [];
   actionMovieResult: any = [];
   adventureMovieResult: any = [];
@@ -18,6 +18,7 @@ export class HomeComponent implements OnInit{
   comedyMovieResult: any = [];
   documentaryMovieResult: any = [];
   sciencefictionMovieResult: any = [];
+  thrillerMovieResult: any = [];
 
   ngOnInit(): void {
     this.bannerData();
@@ -28,18 +29,19 @@ export class HomeComponent implements OnInit{
     this.comedyMovie();
     this.documentaryMovie();
     this.sciencefictionMovie();
+    this.thrillerMovie();
   }
 
   bannerData() {
     this.service.bannerApiData().subscribe((result) => {
-     // console.log(result, 'bannerresult#')
+      // console.log(result, 'bannerresult#')
       this.bannerResult = result.results
     })
   }
 
   trendingData() {
     this.service.trendingMovieApiData().subscribe((result) => {
-    //  console.log(result, 'trendingresult#');
+      //  console.log(result, 'trendingresult#');
       this.trendingMovieResult = result.results;
     });
   }
@@ -77,6 +79,12 @@ export class HomeComponent implements OnInit{
   sciencefictionMovie() {
     this.service.fetchScienceFictionMovies().subscribe((result) => {
       this.sciencefictionMovieResult = result.results;
+    });
+  }
+
+  thrillerMovie() {
+    this.service.fetchThrillerMovies().subscribe((result) => {
+      this.thrillerMovieResult = result.results;
     });
   }
 }
